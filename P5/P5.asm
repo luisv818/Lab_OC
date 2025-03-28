ce43aa5b88ab4477a00dd4d7bb4361e4a1473f6d
luisv818 <luis.vargas95@uabc.edu.mx>
2025-03-28 21:47:28 +0000

    hola

--- P5/P5.asm
+++ P5/P5.asm
@@ -8,8 +8,19 @@ _start:                     ;tell linker entry point
 
 	mov ebx, 0x5C4B2A60
     add ebx, 0x2201213
-    call pHex_dw
-    
+    push bx
+    mov ax, 8
+    mul bl
+    mov cx, [N]
+    mov cx, ax
+    inc cx
+    mov ax, bx
+    mov dx, 0xFF
+    div dx
+    call pHex_w
+    mov ax, dx
+    call pHex_w
+
     mov al, 10
     call putchar
 
@@ -17,5 +28,5 @@ _start:                     ;tell linker entry point
 	mov eax, 1	;system call number (sys_exit) -- fin del programa
 	int 0x80        ;call kernel
 
-    section.data
-    dw N, 0xa,0
+    section .data
+    N dw 0
