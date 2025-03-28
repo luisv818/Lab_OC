d5201e65e53def191dd1f34989c95b9777a9f9ff
luisv818 <luis.vargas95@uabc.edu.mx>
2025-03-28 21:18:07 +0000

    LAB_OC

--- P5/P5.asm
+++ P5/P5.asm
@@ -16,3 +16,6 @@ _start:                     ;tell linker entry point
     push bx
 	mov eax, 1	;system call number (sys_exit) -- fin del programa
 	int 0x80        ;call kernel
+
+    section.data
+    dw N, 0xa,0
